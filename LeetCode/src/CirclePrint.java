0fd024bd60d1dc7ccb6547b338242e3b16c04219
qiang <qiangzhaoo@163.com>
2018-08-06 20:51:13 +0800

    顺时针打印矩阵

--- LeetCode/src/CirclePrint.java
+++ LeetCode/src/CirclePrint.java
@@ -5,50 +5,50 @@ public class CirclePrint {
 
 
     public void circlePrint(int[][] maxtria){
-        int aR = 0;
-        int aC = 0;
-        int bR = maxtria.length - 1;
+        int X1 = 0;
+        int Y1 = 0;
+        int X2 = maxtria.length - 1;
         //列长
-        int bC = maxtria[0].length - 1;
-        while (aR <= bR && aC <= bC){
-            printEdge(maxtria, aR ++, aC ++, bR --, bC --);
+        int Y2 = maxtria[0].length - 1;
+        while (X1 <= X2 && Y1 <= Y2){
+            printEdge(maxtria, X1 ++, Y1 ++, X2 --, Y2 --);
         }
     }
 
 
-    public void  printEdge(int[][] m, int aR, int aC, int bR, int bC ){
-        if(aR == bR){
+    public void  printEdge(int[][] m, int X1, int Y1, int X2, int Y2 ){
+        if(X1 == X2){
             //同一行
-            for (int i = aC; i <= bC; i ++){
-                System.out.print(m[aR][i] + " ");
+            for (int i = Y1; i <= Y2; i ++){
+                System.out.print(m[X1][i] + " ");
             }
-        } else if (aC == bC){
+        } else if (Y1 == Y2){
             //同一列
-            for (int i = aR; i < bR; i ++){
-                System.out.print(m[aC][i] + " ");
+            for (int i = X1; i < X2; i ++){
+                System.out.print(m[Y1][i] + " ");
             }
         }else {
-            int curC = aC;
-            int curR = aR;
-            while (curC != bC) {
+            int curC = Y1;
+            int curR = X1;
+            while (curC != Y2) {
                 //从左向右
-                System.out.print(m[aR][curC] + " ");
+                System.out.print(m[X1][curC] + " ");
                 curC ++;
             }
-            while (curR != bR){
+            while (curR != X2){
                 //从上倒下
-                System.out.print(m[curR][bC] + " ");
+                System.out.print(m[curR][Y2] + " ");
                 curR ++;
             }
 
-            while (curC != aC){
+            while (curC != Y1){
                 //从右向左
-                System.out.print(m[bR][curC] + " ");
+                System.out.print(m[X2][curC] + " ");
                 curC --;
             }
-            while (curR != aR){
+            while (curR != X1){
                 //从下到上
-                System.out.print(m[curR][aC] + " ");
+                System.out.print(m[curR][Y1] + " ");
                 curR --;
             }
         }
